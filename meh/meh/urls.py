83c2bc13fb1db0b02aa544ea82e5bd526b31dcf7
Andrei Coman <andrei.coman@3pillarglobal.com>
2015-05-05 12:24:09 +0300

    Add db settings and admin

--- meh/meh/urls.py
+++ meh/meh/urls.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url(r'^$', include('vermin.urls'))
+urlpatterns = patterns(
+    '',
+    url(r'^', include('vermin.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns(
+        '',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}))
